806c9eacc9aa703a2388030e1cbe5eb73f3fcf0a
Aditya Gupta <adi1998.2010@gmail.com>
2026-05-11 23:29:33 +0530

    tune erebus and ephyra encounters

--- src/athena.lua
+++ src/athena.lua
@@ -4,8 +4,37 @@ local athenaEnconters  = {
             InheritFrom = {"BaseAthenaCombat", "GeneratedF"},
             CanEncounterSkip = false,
 		    PauseDurationAfterPriorityGroup = 0.0,
-            BaseDifficulty = 300,
-		    DepthDifficultyRamp = 100,
+            BaseDifficulty = 250,
+		    DepthDifficultyRamp = 50,
+            MinWaves = 3,
+		    MaxWaves = 3,
+            MinTypes = 3,
+            MaxTypes = 4,
+            MaxTypesCap = 4,
+            ActiveEnemyCapBase = 8,
+		    ActiveEnemyCapMax = 8,
+            MaxEliteTypes = 4,
+            EnemySet = {
+                "Guard_Elite",
+                "Guard",
+                "Guard_Elite",
+
+                "Brawler_Elite",
+                "Brawler",
+                "Brawler_Elite",
+
+                "Radiator_Elite",
+                "Radiator_Elite",
+
+                "Screamer_Elite",
+                "Screamer",
+                "Screamer_Elite",
+
+                "Mage_Elite",
+                "Mage_Elite",
+
+                "SiegeVine_Elite",
+            },
             GameStateRequirements =
             {
                 {
@@ -35,6 +64,9 @@ local athenaEnconters  = {
             InheritFrom = {"BaseAthenaCombat", "GeneratedG"},
             CanEncounterSkip = false,
 		    PauseDurationAfterPriorityGroup = 0.0,
+            MaxEliteTypes = 3,
+            BaseDifficulty = 250,
+            DepthDifficultyRamp = 50,
             GameStateRequirements = {
                 Append = true,
                 {
@@ -60,6 +92,25 @@ local athenaEnconters  = {
         AthenaCombatN = {
             InheritFrom = {"BaseAthenaCombat", "GeneratedN"},
             CanEncounterSkip = false,
+            BaseDifficulty = 500,
+            DepthDifficultyRamp = 120,
+            MaxEliteTypes = 4,
+            ActiveEnemyCapBase = 10,
+		    ActiveEnemyCapMax = 10,
+            MinWaves = 3,
+            MaxWaves = 3,
+            EnemySet = {
+                "Carrion_Elite",
+                "Mudman",
+                "Mudman_Elite",
+                "Zombie_Elite",
+                "ZombieSpawner",
+                "ZombieSpawner_Elite",
+                "ZombieHeavyRanged",
+                "ZombieHeavyRanged_Elite",
+                "ZombieAssassin",
+                "ZombieAssassin_Elite",
+            },
 		    PauseDurationAfterPriorityGroup = 0.0,
             GameStateRequirements = {
                 Append = true,
@@ -118,7 +169,7 @@ if mod.IsZag then
 end
 
 local weight = config.athena.weight
-weight = mod.clampweight(weight)
+-- weight = mod.clampweight(weight)
 
 mod.AddNewEncounters(athenaEnconters, weight, {
     game.NamedRequirementsData.NoRecentFieldNPCEncounter[1].TableValuesToCount
